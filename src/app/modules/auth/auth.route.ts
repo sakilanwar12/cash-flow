329cba07bdd549619447f4ee7df3df053fc12fba
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-08-09 16:55:47 +0600

    add authentication middleware to refresh-token and logout routes

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { AuthControllers } from "./auth-controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 const router = Router();
 
-
 router.post("/login", AuthControllers.credentialsLogin);
-router.post("/refresh-token", AuthControllers.getNewAccessToken);
-router.post("/logout", AuthControllers.logOut);
+router.post(
+  "/refresh-token",
+  checkAuth(Role.USER, Role.ADMIN, Role.AGENT),
+  AuthControllers.getNewAccessToken
+);
+router.post(
+  "/logout",
+  checkAuth(Role.USER, Role.ADMIN, Role.AGENT),
+  AuthControllers.logOut
+);
 export const AuthRoutes = router;
